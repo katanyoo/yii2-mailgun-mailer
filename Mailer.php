f3869ea06c4c63ac0eb39f2abb8c9cb0e5c4a81e
Katanyoo Ubalee <ubalee.k@gmail.com>
2014-11-18 02:14:14 +0700

    rename class

---
+++ Mailer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace katanyoo\mailer;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\mail\BaseMailer;
+/**
+ * Mailer implements a mailer based on Mailgun.
+ */
+class Mailer extends BaseMailer
+{
+	/**
+	 * @inheritdoc
+	 */
+	protected function sendMessage($message)
+	{
+
+	}
+}
---
+++ Message.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace katanyoo\mailer;
+
+use yii\mail\BaseMessage;
+
+/**
+ * Message implements a message class based on Mailgun.
+ *
+ *
+ * @method Mailer getMailer() returns mailer instance.
+ *
+ *
+ * @author Katanyoo Ubalee <ublee.k@gmail.com>
+ */
+class Message extends BaseMessage
+{
+
+}
